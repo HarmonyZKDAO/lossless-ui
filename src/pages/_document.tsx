1ac51f10a589023b561ea214c8fdbf4558358bf6
vplasencia <v.pcalana@gmail.com>
2022-09-12 17:35:57 -0400

    Change social media image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,10 +35,7 @@ class MyDocument extends Document {
             <meta property='og:site_name' content={title} />
             <meta property='og:url' content={url} />
             <meta property='og:type' content='website' />
-            <meta
-              property='og:image'
-              content={`${url}/pooltogether-facebook-share-image-1200-630@2x.png`}
-            />
+            <meta property='og:image' content={`${url}/socialMedia.png`} />
             <meta property='og:rich_attachment' content='true' />
             <meta property='og:image:width' content='1200' />
             <meta property='og:image:height' content='630' />
@@ -47,10 +44,7 @@ class MyDocument extends Document {
             <meta property='twitter:description' content={description} />
             <meta property='twitter:card' content='summary_large_image' />
             <meta property='twitter:site' content={twitterHandle} />
-            <meta
-              property='twitter:image:src'
-              content={`${url}/pooltogether-twitter-share-image-1200-675@2x.png`}
-            />
+            <meta property='twitter:image:src' content={`${url}/socialMedia.png`} />
             <meta property='twitter:url' content={url} />
             <meta property='twitter:creator' content={twitterHandle} />
 
