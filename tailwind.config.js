8e5dae3e3b45deb48b34b67de494e796d8c91588
vplasencia <v.pcalana@gmail.com>
2022-09-18 14:20:01 -0400

    Add colors tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = pooltogetherTheme({
         'indigo900': '#312e81',
         'indigo800': '#3730a3',
         'indigo700':'#4338ca',
-        'indigo600': '#4f46e5'
+        'indigo600': '#4f46e5',
+        'blue-2': '#458fff',
+        'blue900': '#1e3a8a',
+        'blue800': '#1e40af',
+        'blue700': '#1d4ed8'
       }
     },
     variants: {},
