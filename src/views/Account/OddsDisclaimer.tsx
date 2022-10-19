be8e68fab5af4699046e46ab710ab670f516cd32
vplasencia <v.pcalana@gmail.com>
2022-10-19 13:05:01 -0400

    Change text Account Odds Disclaimer

--- src/views/Account/OddsDisclaimer.tsx
+++ src/views/Account/OddsDisclaimer.tsx
@@ -5,14 +5,14 @@ export const OddsDisclaimer = (props: { className?: string }) => {
   const { t } = useTranslation()
   return (
     <span className={classNames('opacity-40 text-xxxs text-center mx-auto', props.className)}>
-      * <span>{t('oddsDisclaimer')}</span>
+      * <span>{t('Odds Disclaimer')}</span>
       <a
         href='https://docs.pooltogether.com/welcome/faq#prizes-and-winning'
         target='_blank'
         rel='noopener noreferrer'
         className='underline ml-1 text-xxxs'
       >
-        {t('readMore')}
+        {t('Read More')}
       </a>
     </span>
   )
