1aeeccb30ab474cd69b43cbb91a3bf5d3500f0d4
vplasencia <v.pcalana@gmail.com>
2022-10-31 20:22:03 -0400

    Add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = pooltogetherTheme({
         'indigo900': '#312e81',
         'indigo800': '#3730a3',
         'indigo700':'#4338ca',
+        'indigo300': "#a5b4fc",
         'indigo600': '#4f46e5',
         'blue-2': '#458fff',
         'blue900': '#1e3a8a',
