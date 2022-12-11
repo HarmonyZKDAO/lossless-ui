a5bd5f9439bcf75073fbed0c07621660c445c8de
vplasencia <v.pcalana@gmail.com>
2022-12-11 18:48:02 -0500

    Add Connect Wallet Terms and Disclaimer

--- src/components/Layout/FullWalletConnectionButtonWrapper.tsx
+++ src/components/Layout/FullWalletConnectionButtonWrapper.tsx
@@ -15,28 +15,40 @@ export const FullWalletConnectionButtonWrapper = (props) => {
     <FullWalletConnectionButton
       {...props}
       chains={chains}
+      // TosDisclaimer={
+      //   <Trans
+      //     i18nKey='connectWalletTermsAndDisclaimerBlurb'
+      //     components={{
+      //       termsLink: (
+      //         <a
+      //           className='text-pt-teal transition hover:opacity-70 underline'
+      //           href='https://pooltogether.com/terms/'
+      //           target='_blank'
+      //           rel='noreferrer'
+      //         />
+      //       ),
+      //       disclaimerLink: (
+      //         <a
+      //           className='text-pt-teal transition hover:opacity-70 underline'
+      //           href='https://pooltogether.com/protocol-disclaimer/'
+      //           target='_blank'
+      //           rel='noreferrer'
+      //         />
+      //       )
+      //     }}
+      //   />
+      // }
+      // TosDisclaimer="By connecting a wallet, you agree to Lossless's Terms of Service and acknowledge that you have read and understand the Lossless protocol disclaimer."
       TosDisclaimer={
-        <Trans
-          i18nKey='connectWalletTermsAndDisclaimerBlurb'
-          components={{
-            termsLink: (
-              <a
-                className='text-pt-teal transition hover:opacity-70 underline'
-                href='https://pooltogether.com/terms/'
-                target='_blank'
-                rel='noreferrer'
-              />
-            ),
-            disclaimerLink: (
-              <a
-                className='text-pt-teal transition hover:opacity-70 underline'
-                href='https://pooltogether.com/protocol-disclaimer/'
-                target='_blank'
-                rel='noreferrer'
-              />
-            )
-          }}
-        />
+        <span>
+          By connecting a wallet, you agree to Lossless's{' '}
+          <a className='text-blue-2 transition hover:opacity-70 underline'>Terms of Service</a> and
+          acknowledge that you have read and understand the{' '}
+          <a className='text-blue-2 transition hover:opacity-70 underline'>
+            Lossless protocol disclaimer
+          </a>
+          .
+        </span>
       }
     />
   )
